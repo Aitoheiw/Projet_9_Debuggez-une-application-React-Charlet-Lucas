6bbd5225e71c676ab2fd2c03296dd60b06874162
Aitoheiw <124202509+Aitoheiw@users.noreply.github.com>
2025-08-08 08:16:32 +0200

    fix(context): utilise la date ISO pour retourner le dernier événement dans `last`"

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -31,13 +31,42 @@ export const DataProvider = ({ children }) => {
     getData();
   });
 
+  console.log("✅ EVENTS", data?.events);
+  console.log(
+    "✅ LAST CALCULATED",
+    [...(data?.events || [])].sort((a, b) => {
+      const monthsMap = {
+        janvier: 0,
+        février: 1,
+        mars: 2,
+        avril: 3,
+        mai: 4,
+        juin: 5,
+        juillet: 6,
+        août: 7,
+        septembre: 8,
+        octobre: 9,
+        novembre: 10,
+        décembre: 11,
+      };
+
+      const getIndex = (month) => monthsMap[month?.trim().toLowerCase()] ?? -1;
+
+      return getIndex(b.month) - getIndex(a.month);
+    })[0]
+  );
+
   return (
     <DataContext.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
       value={{
         data,
         error,
-        last: data?.events?.at(-1) || null, // Fix :  Ajout de `last` pour permettre d'accéder à la dernière prestation dans la page home
+        last:
+          [...(data?.events || [])].sort(
+            (a, b) => new Date(b.date) - new Date(a.date)
+          )[0] || null,
+        // Fix :  Ajout de `last` pour permettre d'accéder à la dernière prestation dans la page home
       }}
     >
       {children}
